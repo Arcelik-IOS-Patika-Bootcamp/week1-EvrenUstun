95aa9bf8d45ea097f5d93cfbb07d24ef3ec60d63
Evren Ustun <ustunnevren@gmail.com>
2022-05-17 11:36:17 +0300

    Enums added.
    Enum example added.

---
+++ week1-EvrenUstun.playground/Contents.swift
@@ -0,0 +1,24 @@
+import UIKit
+
+// enum I created for engine manufacturers.
+enum EngineManufacturers{
+    case Honda
+    case Mercedes
+    case Ferrari
+}
+
+// enum I created for tyre type.
+enum TyreType{
+    case Soft
+    case Medium
+    case Hard
+    case Intermediate
+    case Wet
+}
+
+// enum I created for calendar events.
+enum CalendarEvent{
+    case Practice
+    case Qualify
+    case Race
+}
