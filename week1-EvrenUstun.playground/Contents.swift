2effe67693489f4661d029daeb695671226d846c
Evren Ustun <ustunnevren@gmail.com>
2022-05-17 11:39:57 +0300

    Struct added.
    Struct example added.

--- week1-EvrenUstun.playground/Contents.swift
+++ week1-EvrenUstun.playground/Contents.swift
@@ -22,3 +22,16 @@ enum CalendarEvent{
     case Qualify
     case Race
 }
+
+// struct for calendar.
+struct Calendar{
+    let everyFriday:CalendarEvent = .Practice
+    let everySaturday:CalendarEvent = .Qualify
+    let everySunday:CalendarEvent = .Race
+}
+
+// See F1 calendar
+let calendar = Calendar()
+
+print("Formula 1 Program")
+print("Friday: \(calendar.everyFriday) \nSaturday: \(calendar.everySaturday) \nSunday: \(calendar.everySunday)\n")
